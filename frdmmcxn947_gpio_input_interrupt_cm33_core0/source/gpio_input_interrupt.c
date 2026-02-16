b722cd01ec9b1ac8bff49183d8949687adc4fb0f
cobiansm <sandra_smca@hotmail.com>
2026-02-16 01:44:54 -0600

    Working state machine with RGB led states

--- frdmmcxn947_gpio_input_interrupt_cm33_core0/source/gpio_input_interrupt.c
+++ frdmmcxn947_gpio_input_interrupt_cm33_core0/source/gpio_input_interrupt.c
@@ -22,16 +22,35 @@
 /*******************************************************************************
  * Definitions
  ******************************************************************************/
+// function type for each state
+typedef void (*active_state_s)(void *);
 
+// structure of a state
+typedef struct ledState{
+    active_state_s led_state;
+    struct ledState *next;
+    struct ledState *prev;
+    const char *name;
+} ledState;
 /*******************************************************************************
  * Prototypes
  ******************************************************************************/
 void BOARD_InitHardware(void);
+
+// state functions' prototypes
+void red_led_state(void *ptr);
+void green_led_state(void *ptr);
+void blue_led_state(void *ptr);
 /*******************************************************************************
  * Variables
  ******************************************************************************/
 volatile bool sw3_ButtonPress = false;
 volatile bool sw2_ButtonPress = false;
+
+// declaration of the states' variables
+static ledState red_led_state_s;
+static ledState green_led_state_s;
+static ledState blue_led_state_s;
 /*******************************************************************************
  * Code
  ******************************************************************************/
@@ -43,14 +62,83 @@ volatile bool sw2_ButtonPress = false;
 void GPIO00_IRQHandler(void)
 {
 	if (GPIO_PinGetInterruptFlag(GPIO0, BOARD_SW3_GPIO_PIN)) {
-		GPIO_PinClearInterruptFlag(GPIO0, BOARD_SW3_GPIO_PIN);
 		sw3_ButtonPress = true;
+        GPIO_PinClearInterruptFlag(GPIO0, BOARD_SW3_GPIO_PIN);
 	} if (GPIO_PinGetInterruptFlag(GPIO0, BOARD_SW2_GPIO_PIN)) {
-		GPIO_PinClearInterruptFlag(GPIO0, BOARD_SW2_GPIO_PIN);
 		sw2_ButtonPress = true;
+        GPIO_PinClearInterruptFlag(GPIO0, BOARD_SW2_GPIO_PIN);
 	}
 
     SDK_ISR_EXIT_BARRIER;
+}
+
+/*
+* Red LED state
+*/
+void red_led_state(void *ptr) {
+    LED_RED_ON();
+    LED_BLUE_OFF();
+    LED_GREEN_OFF();
+}
+
+/*
+* Green LED state
+*/
+void green_led_state(void *ptr) {
+    LED_GREEN_ON();
+    LED_RED_OFF();
+    LED_BLUE_OFF();
+}
+
+/*
+* Blue LED state
+*/
+void blue_led_state(void *ptr) {
+    LED_RED_OFF();
+    LED_GREEN_OFF();
+    LED_BLUE_ON();
+}
+
+/* 
+* Function to initialize the states of the state machine
+*/
+static void states_init(void)
+{
+    red_led_state_s.led_state  = red_led_state;
+    red_led_state_s.next    = &green_led_state_s;    
+    red_led_state_s.prev    = &blue_led_state_s;   
+    red_led_state_s.name    = "RED";
+
+    green_led_state_s.led_state  = green_led_state;
+    green_led_state_s.next    = &blue_led_state_s;    
+    green_led_state_s.prev    = &red_led_state_s;  
+    green_led_state_s.name    = "GREEN";
+
+    blue_led_state_s.led_state  = blue_led_state;
+    blue_led_state_s.next    = &red_led_state_s;    
+    blue_led_state_s.prev    = &green_led_state_s;  
+    blue_led_state_s.name    = "BLUE";
+}
+
+/*
+* Change between states, SW3 goes forward, SW2 goes backwards. No switch statement, just pointers.
+*/
+static void state_machine_step(ledState **current, bool sw2_pressed, bool sw3_pressed, void *ptr)
+{
+    if (current == NULL || *current == NULL) return;
+
+    /* Execute current state */
+    PRINTF("Entering state: %s\r\n", (*current)->name);
+    (*current)->led_state(ptr);
+
+    /* Change forwards or backwards */
+    if (sw3_pressed && (*current)->next) {
+        *current = (*current)->next;
+        sw3_ButtonPress = false;
+    } else if (sw2_pressed && (*current)->prev) {
+        *current = (*current)->prev;
+        sw2_ButtonPress = false;
+    }
 }
 
 /*!
@@ -73,7 +161,7 @@ int main(void)
     BOARD_InitHardware();
 
     /* Print a note to terminal. */
-    PRINTF("\r\n GPIO Driver example\r\n");
+    PRINTF("\r\n State Machine \r\n");
 
     /* Init input switch GPIO. */
     GPIO_PinInit(BOARD_SW3_GPIO, BOARD_SW3_GPIO_PIN, &sw_config);
@@ -87,30 +175,15 @@ int main(void)
     EnableIRQ(GPIO00_IRQn);
 
     /* Init output LED GPIO. */
-    GPIO_PinInit(BOARD_LED_RED_GPIO, BOARD_LED_RED_GPIO_PIN, &led_config);
+    LED_RED_INIT(LOGIC_LED_OFF);
+    LED_GREEN_INIT(LOGIC_LED_OFF);
     GPIO_PinInit(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_GPIO_PIN, &led_config);
 
-    GPIO_PortSet(GPIO0, 1U << BOARD_LED_RED_GPIO_PIN);
-    GPIO_PortSet(GPIO0, 1U << BOARD_LED_BLUE_GPIO_PIN);
-
-    GPIO_PinWrite(BOARD_LED_RED_GPIO, BOARD_LED_RED_GPIO_PIN, 1);
-    GPIO_PinWrite(BOARD_LED_BLUE_GPIO, BOARD_LED_BLUE_GPIO_PIN, 1);
+    states_init();
+    ledState *current_state = &red_led_state_s;
 
     while (1)
     {
-        if (sw3_ButtonPress)
-        {
-            PRINTF(" %s is pressed \r\n", BOARD_SW3_NAME);
-            /* Toggle LED. */
-            GPIO_PortToggle(BOARD_LED_RED_GPIO, 1U << BOARD_LED_RED_GPIO_PIN);
-            /* Reset state of button. */
-            sw3_ButtonPress = false;
-        } if (sw2_ButtonPress) {
-        	PRINTF(" %s is pressed \r\n", BOARD_SW2_NAME);
-			/* Toggle LED. */
-			GPIO_PortToggle(BOARD_LED_BLUE_GPIO, 1U << BOARD_LED_BLUE_GPIO_PIN);
-			/* Reset state of button. */
-			sw2_ButtonPress = false;
-        }
+        state_machine_step(&current_state, sw2_ButtonPress, sw3_ButtonPress, NULL);
     }
 }
